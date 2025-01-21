0f8ba071b46ed0c559da882fe10cdcd9da0a56ee
Yan <1786764838@qq.com>
2025-01-21 11:30:37 +0800

    chore(dashboard): update ESLint configuration to support browser and Node.js environments
    - Added environment settings for browser, Node.js, and ES6 support in .eslintrc.js.
    - These changes enhance the linting process by ensuring compatibility with modern JavaScript features and environments.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,11 @@ module.exports = {
     ecmaVersion: 2020, // 支持可选链操作符
     sourceType: "module", // 支持 ES6 模块
   },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   // 其他配置
   rules: {
     // 其他规则
